8a008daa99ef7fe88ba12d9ff915dbec8809948e
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-06 21:12:01 -0500

    day 3

---
+++ Burger/Burger.swift
@@ -0,0 +1,35 @@
+//
+//  Burger.swift
+//  Burger
+//
+//  Created by Ella Madalinski on 10/6/21.
+//
+
+import Foundation
+import UIKit
+
+public class Burger{
+    
+    //var type = getType()
+    
+    
+    enum burgerTypes: String{
+        case chicago, atlanta, hawaiian, wisconsin
+    }
+    enum burgerCooked: String{
+        case rare, medium, mediumWell, well
+    }
+    
+    init(type: String, cooked: String){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
+
--- Burger/ViewController.swift
+++ Burger/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var type = 0
+    var cooked = 0
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func typeSegControl(_ sender: UISegmentedControl) {
+        type = sender.selectedSegmentIndex
+        
+        
+        
+    }
+    
+    
+    @IBAction func cookedSegControl(_ sender: UISegmentedControl) {
+        cooked = sender.selectedSegmentIndex
+        
+        
+    }
+    
+    func getType()->Int{
+        return type
+    }
+    func getCooked()->Int{
+        return cooked
+    }
+    
 }
 
